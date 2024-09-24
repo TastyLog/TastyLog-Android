3e90380bf9f884a7dbc09fc415373b9ce78c6efa
Moonsu Kang <rkdms4971@naver.com>
2024-09-24 21:34:46 +0900

    fix : edit root & app build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,15 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.knu.retastylog.application")
 }
 
 android {
     namespace = "com.knu.retastylog"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.knu.retastylog"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.appVersion.get()
     }
 
     buildTypes {
@@ -23,21 +17,14 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
 dependencies {
-
+    implementation(project(":core:common"))
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
 allprojects {
     apply {
@@ -20,5 +21,10 @@ buildscript {
         google()
         mavenCentral()
         maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath(libs.kotlin.gradleplugin)
+        classpath(libs.hilt.plugin)
+        classpath(libs.android.gradle.plugin)
     }
 }
