522e1409216e3a1580000676441d6864dea0eb57
Moonsu Kang <rkdms4971@naver.com>
2024-10-02 19:22:29 +0900

    mod : rename restaurant di module

--- data/home/src/main/java/com/knu/home/di/RestaurantDataSourceModule.kt
+++ data/home/src/main/java/com/knu/home/di/RestaurantDataSourceModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataSourceModule {
+object RestaurantDataSourceModule {
 
     @Provides
     @Singleton
--- data/home/src/main/java/com/knu/home/di/RestaurantRepositoryModule.kt
+++ data/home/src/main/java/com/knu/home/di/RestaurantRepositoryModule.kt
@@ -12,7 +12,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+object RestaurantRepositoryModule {
 
     @Provides
     @Singleton
