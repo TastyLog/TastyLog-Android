7b1a7283a9c102f9aca7f41ca4362b89f0947167
Moonsu Kang <rkdms4971@naver.com>
2024-10-02 18:46:45 +0900

    add : add youtuberList Api Route

--- core/network/src/main/java/com/knu/network/api/ApiRoutes.kt
+++ core/network/src/main/java/com/knu/network/api/ApiRoutes.kt
@@ -2,4 +2,5 @@ package com.knu.network.api
 
 object ApiRoutes {
     const val GET_RESTAURANT_LIST = "/api/v1/food/{latitude}/{longitude}"
+    const val GET_YOUTUBER_LIST = "/api/v1/youtuber/all-list"
 }
