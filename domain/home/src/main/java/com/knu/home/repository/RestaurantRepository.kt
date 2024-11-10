71b3fbcd87236a9638832685720eda0bcdb041dc
Moonsu Kang <rkdms4971@naver.com>
2024-11-10 20:56:07 +0900

    refactor : add page & size

--- domain/home/src/main/java/com/knu/home/repository/RestaurantRepository.kt
+++ domain/home/src/main/java/com/knu/home/repository/RestaurantRepository.kt
@@ -4,5 +4,10 @@ import com.knu.home.entity.RestaurantEntity
 import kotlinx.coroutines.flow.Flow
 
 interface RestaurantRepository {
-    suspend fun getRestaurantList(latitude: Double, longitude: Double): Flow<List<RestaurantEntity>>
+    suspend fun getRestaurantList(
+        latitude: Double,
+        longitude: Double,
+        page: Int = 0,   // 기본값 0
+        size: Int = 1000   // 기본값 1000
+    ): Flow<List<RestaurantEntity>>
 }
