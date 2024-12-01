c5b940780c24e2009facc8f575ed03ebf0460c65
Moonsu Kang <rkdms4971@naver.com>
2024-12-02 01:47:40 +0900

    add : add get,set String func

--- core/datastore/src/main/java/com/knu/datastore/preferences/SharedPreferencesManager.kt
+++ core/datastore/src/main/java/com/knu/datastore/preferences/SharedPreferencesManager.kt
@@ -1,21 +1,30 @@
 package com.knu.datastore.preferences
 
 import android.content.SharedPreferences
-import com.knu.datastore.preferences.SharedPreferencesKeys.KEY_HAS_COMPLETED_INTRO
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SharedPreferencesManager
-    @Inject
-    constructor(
-        private val sharedPreferences: SharedPreferences,
-    ) {
-        fun setIntroCompleted(completed: Boolean) {
-            sharedPreferences.edit().putBoolean(KEY_HAS_COMPLETED_INTRO, completed).apply()
-        }
-
-        fun hasCompletedIntro(): Boolean {
-            return sharedPreferences.getBoolean(KEY_HAS_COMPLETED_INTRO, false)
-        }
+@Inject
+constructor(
+    private val sharedPreferences: SharedPreferences,
+) {
+    fun setIntroCompleted(completed: Boolean) {
+        sharedPreferences.edit().putBoolean(SharedPreferencesKeys.KEY_HAS_COMPLETED_INTRO, completed).apply()
     }
+
+    fun hasCompletedIntro(): Boolean {
+        return sharedPreferences.getBoolean(SharedPreferencesKeys.KEY_HAS_COMPLETED_INTRO, false)
+    }
+
+    fun putStringList(key: String, list: List<String>) {
+        sharedPreferences.edit().putStringSet(key, list.toSet()).apply()
+    }
+
+
+    fun getStringList(key: String): List<String> {
+        val stringSet = sharedPreferences.getStringSet(key, emptySet()) ?: emptySet()
+        return stringSet.toList()
+    }
+}
